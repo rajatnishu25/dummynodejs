37c53899a519b2b5f1b969ab4ee059ce48cc126d
Rajat Chourasiya <rajat@rajat.chourasiya>
2021-02-23 18:41:26 +0530

    inital commit using dotenv modules

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+var app = express();
+const mongoose = require('mongoose');
+
+
+require('dotenv').config();
+// {path: path/filename}
+
+// process.evn.Port
+
+let port = process.env.PORT;
+let dburl = process.env.url;
+
+
+mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true }).then(function () {
+  console.log("Connect Datebase");
+}, function (err) {
+  console.log(err)
+})
+
+
+app.get('/data', (req, res) => {
+  res.send(importData)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app is listening on port ${port}`);
+})
+
+
+
+
+
+
